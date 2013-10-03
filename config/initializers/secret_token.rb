fbf3a4bd858a36776a122ca0a5fbfc72ac663697
Mason Packard <masonpackard@MasonsMacBook.local>
2013-10-03 17:46:51 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BioApp::Application.config.secret_key_base = '3c2607dd608e9b5cd5b05e6e5c1d6069cd1e55e10f17ec5cfee45b265a9386ad06a0a78f8f0e31d8d2f31ea45d3b0b62abec72607e6ef0ae5486e94bd19f6b09'
