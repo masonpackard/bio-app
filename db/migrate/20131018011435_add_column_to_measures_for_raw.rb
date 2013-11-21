91d20a35fb6e9611236671679bb855e294763d68
masonpackard <mason.packard@yahoo.com>
2013-11-20 21:30:03 -0500

    Cleaning up

--- db/migrate/20131018011435_add_column_to_measures_for_raw.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToMeasuresForRaw < ActiveRecord::Migration
-  def change
-  	add_column :measures, :measure_raw, :text
-  end
-end
--- db/migrate/20131024234126_add_oauth_secret_to_authentications.rb
+++
@@ -1,5 +0,0 @@
-class AddOauthSecretToAuthentications < ActiveRecord::Migration
-  def change
-  	add_column :authentications, :oauth_token_secret, :string
-  end
-end
