b0dc9f87d5a83a5cc761423358f39e7de4d83fcc
masonpackard <mason.packard@yahoo.com>
2014-01-31 21:50:35 -0500

    adding in information for healthpro for profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140130193534) do
+ActiveRecord::Schema.define(version: 20140201023403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20140130193534) do
     t.string   "organization"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",        default: ""
   end
 
   create_table "invitations", force: true do |t|
