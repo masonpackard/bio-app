ce91cffce9e870ecff2dde60d94494ff187adecd
masonpackard <mason.packard@yahoo.com>
2014-08-24 16:15:16 -0400

    DRY up and make copyright dynamic.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,11 @@ module ApplicationHelper
 	def devise_mapping
 		@devise_mapping ||= Devise.mappings[:user]
 	end
+
+	def copyright
+		start_year = 2013
+		current_year = Time.new.year
+
+		"#{start_year} - #{current_year}"
+	end
 end
